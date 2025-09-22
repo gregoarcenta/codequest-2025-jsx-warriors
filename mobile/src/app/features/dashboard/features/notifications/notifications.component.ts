a0e723e3cde91947489d14972a4663fb6e1073a5
Gregory Arcentales <gregoarcenta@gmail.com>
2025-09-21 23:02:45 -0500

    delete mobile folder

--- mobile/src/app/features/dashboard/features/notifications/notifications.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-notifications',
-  templateUrl: './notifications.component.html',
-  styleUrls: ['./notifications.component.scss'],
-})
-export default class NotificationsComponent { }
--- mobile/src/app/features/dashboard/features/saved/saved.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-saved',
-  templateUrl: './saved.component.html',
-  styleUrls: ['./saved.component.scss'],
-})
-export default class SavedComponent { }
